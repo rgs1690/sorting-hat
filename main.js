df0e919bbe452da450c54071457a94cbf0501ed2
Grace Sutherland <rgs1690@gmail.com>
2021-08-01 16:18:50 -0500

    created a form and got it to render to the DOM when the Lets begin button is clicked

--- main.js
+++ main.js
@@ -2,3 +2,54 @@ const renderToDom =(divId, textToRender) => {
     const selectedDiv= document.querySelector(divId);
     selectedDiv.innerHTML = textToRender;
 };
+
+const introCard = () => {
+    const domString =`
+    <div class="card">
+     <div class="card-body">
+            <h5 class="card-title">Welcome to Hogwarts!</h5>
+            <p class="card-text">"Now slip me snug around your ears,<br>
+            I've never yet been wrong,<br>
+            I'll have alook inside your mind<br>
+            And tell where you belong!”.</p>
+            <button type ="button" id="letsBegin" class="btn btn-primary">Let's begin!</button>
+        </div>
+    </div>
+    `
+    renderToDom("#introCard", domString);
+};
+
+const createForm = () => {
+    const domString = `
+    <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Student's Name:</label>
+      <input type="text" class="form-control" id="input" aria-describedby="studentsName">
+      <div id="formText" class="form-text">Type your name into the field and I'll tell you what house you belong in!</div>
+    <button type="submit" id="sort"class="btn btn-primary">Start Sorting!</button>
+  </form>
+     `
+
+    renderToDom("#sortForm", domString);
+};
+
+
+const addForm =(event) => {
+    const targetId = event.target.id;
+    const targetType = event.target.type;
+    if (targetType === "button"){
+        createForm();
+    };
+};
+
+const buttonEvents = () => {
+    document.querySelector("#introCard").addEventListener("click", addForm);
+
+};
+
+const startApp = () => {
+    introCard();
+    buttonEvents();
+};
+
+startApp(); 
